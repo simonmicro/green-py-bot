3a2d3724320e7ae1819cca501a524bfbdb2b3920
Simon Beginn <simon@simonmicro.de>
2020-06-04 20:19:48 +0200

    Added repo logic

---
+++ greenbot/repos.py
@@ -0,0 +1,29 @@
+import os
+import hashlib
+import greenbot.config
+import git
+import logging
+
+reposPath = 'repos'
+
+def update():
+    global reposPath
+    try:
+        os.mkdir(reposPath)
+    except FileExistsError:
+        pass
+    # Update all copies of the remote repos
+    for repoUrl in greenbot.config.repos:
+        # Generate the repo name based on the URI
+        m = hashlib.sha256()
+        m.update(repoUrl.encode())
+        repoName = m.hexdigest()
+        repoPath = os.path.join(reposPath, repoName)
+        if os.path.isdir(repoPath):
+            # Dir is already there -> just update it
+            logging.info('Updating ' + repoUrl)
+            git.Git(repoPath).pull()
+        else:
+            logging.info('Cloning ' + repoUrl)
+            os.mkdir(repoPath)
+            git.Git().clone(repoUrl, repoPath)
