b265c179efc79653955a5881341379bcb0d3feb4
Simon Beginn <simon@simonmicro.de>
2020-06-04 20:19:20 +0200

    Added repos to config and formatted default output

--- greenbot/config.py
+++ greenbot/config.py
@@ -3,9 +3,11 @@ import os
 import json
 
 token = ''
+repos = []
 
 def load():
     global token
+    global repos
     fileName = 'config.json'
     logging.debug('Loading config from ' + fileName)
     # Test if file is there...
@@ -13,14 +15,16 @@ def load():
         logging.debug('Config file not found. Creating default...')
         # If not: Create default
         defaultConfig = {
-            'token' : ''
+            'token' : '',
+            'repos' : ['css']
         }
         f = open(fileName, 'w')
-        f.write(json.dumps(defaultConfig))
+        f.write(json.dumps(defaultConfig, sort_keys=True, indent=4))
         f.close()
 
     # And load it...
     with open(fileName) as file:
         config = json.loads(file.read())
         token = config['token']
+        repos = config['repos']
     logging.debug('Config loaded')
