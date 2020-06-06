5e6118b3f5efc1485b667f50d50211f9c650151c
Simon Beginn <simon@simonmicro.de>
2020-06-07 01:18:48 +0200

    Added more functions

--- greenbot/user.py
+++ greenbot/user.py
@@ -1,18 +1,27 @@
+import json
+
 userPath = 'data/user'
+
+# Make sure the data path exists
+os.makedirs(userPath, exist_ok=True)
 
 class User:
     uid = None
+    scripts = []
 
     def __init__(self, uid):
+        global userPath
         self.uid = uid
 
-    def getConfig(self):
-        global userPath
-        logging.debug('Getting user data repos')
-        os.makedirs(userPath, exist_ok=True)
-        return {}
+        # We'll default config if nothing is found
+        logging.debug('Getting user data for ' + int(uid))
+        fileName = os.path.join(userPath, int(uid) + '.json')
+        if os.path.isfile(fileName):
+            with open(fileName) as file:
+                config = json.loads(file.read())
+                self.scripts = config['scripts']
 
-    def saveConfig(self, config):
+    def __save(self):
         return
 
     def activateScript(self, repo, script):
@@ -20,6 +29,3 @@ class User:
 
     def deactivateScript(self, repo, script):
         return
-
-def get(uid):
-    return User(uid)
