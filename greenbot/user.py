1a77c04f9a8b81395393100e647888d119c1fa93
Simon Beginn <simon@simonmicro.de>
2020-06-07 02:33:10 +0200

    Prefixed private members

--- greenbot/user.py
+++ greenbot/user.py
@@ -8,44 +8,44 @@ userPath = 'data/user'
 os.makedirs(userPath, exist_ok=True)
 
 class User:
-    uid = None
-    scripts = []
+    __uid = None
+    __scripts = []
 
     def __init__(self, uid):
-        self.uid = int(uid)
+        self.__uid = int(uid)
 
         # We'll use the default config if nothing is found
-        logging.debug('Getting user data for ' + str(uid))
+        logging.debug('Getting user data for ' + str(self.__uid))
         if os.path.isfile(self.__getConfigFileName()):
             with open(self.__getConfigFileName()) as file:
                 config = json.loads(file.read())
-                self.scripts = list(set(config['scripts']))
+                self.__scripts = list(set(config['scripts']))
 
     def __getConfigFileName(self):
         global userPath
-        return os.path.join(userPath, str(self.uid) + '.json')
+        return os.path.join(userPath, str(self.__uid) + '.json')
 
     def __write(self):
         f = open(self.__getConfigFileName(), 'w')
         writeme = {
-            'scripts' : self.scripts
+            'scripts' : self.__scripts
         }
         f.write(json.dumps(writeme, sort_keys=True, indent=4))
         f.close()
         return
 
     def activateScript(self, repo, script):
-        self.scripts.append(repo + '/' + script)
+        self.__scripts.append(repo + '/' + script)
         self.__write()
         return
 
     def deactivateScript(self, repo, script):
-        self.scripts.remove(repo + '/' + script)
+        self.__scripts.remove(repo + '/' + script)
         self.__write()
         return
 
     def getScripts(self):
         returnme = []
-        for path in self.scripts:
+        for path in self.__scripts:
             returnme.append(path.split('/'))
         return returnme
