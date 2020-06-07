18576bab097674368faed1848dca794239aac288
Simon Beginn <simon@simonmicro.de>
2020-06-07 02:26:14 +0200

    Removed old function

--- greenbot/util.py
+++ greenbot/util.py
@@ -7,13 +7,3 @@ def updateOrReply(update, *args, **kwargs):
     else:
         # Okay, no callback -> reply with new msg
         return update.effective_message.reply_text(*args, **kwargs)
-
-def getChatID(update):
-    print(update.effective_chat)
-    # Detect if we are inside a callback or just inside a handler
-    if update.effective_chat is not None:
-        # No callback
-        return update.effective_chat.id
-    else:
-        # Triggered by callback? Just try an other approach
-        return update.callback_query.message.chat.id
