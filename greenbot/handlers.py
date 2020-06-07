2c61092920d914de1fbd8c34f79216ce330a4519
Simon Beginn <simon@simonmicro.de>
2020-06-07 02:34:49 +0200

    Fixed info

--- greenbot/handlers.py
+++ greenbot/handlers.py
@@ -34,7 +34,7 @@ def script_info(update, context):
 
 def user_info(update, context):
     logging.debug('Command: user_info')
-    context.bot.send_message(chat_id=update.effective_chat.id, text='User info: ' + str(greenbot.user.User(update.message.chat.id).scripts))
+    context.bot.send_message(chat_id=update.effective_chat.id, text='User info: ' + str(greenbot.user.User(update.message.chat.id).getScripts()))
 
 def activate(update, context):
     logging.debug('Command: activate')
