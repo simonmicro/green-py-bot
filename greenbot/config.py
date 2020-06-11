1caffd8edd52fe9144ad38734c3897f96d0e39fd
Simon Beginn <simon@simonmicro.de>
2020-06-12 00:57:21 +0200

    Moved config into data dir

--- greenbot/config.py
+++ greenbot/config.py
@@ -4,26 +4,30 @@ import json
 
 token = ''
 repos = {}
+configPath = 'data/config.json'
+
+# Make sure the data path exists
+os.makedirs(os.path.split(configPath)[0], exist_ok=True)
 
 def load():
+    global configPath
     global token
     global repos
-    fileName = 'config.json'
-    logging.debug('Loading config from ' + fileName)
+    logging.debug('Loading config from ' + configPath)
     # Test if file is there...
-    if not os.path.isfile(fileName):
+    if not os.path.isfile(configPath):
         logging.debug('Config file not found. Creating default...')
         # If not: Create default
         defaultConfig = {
             'token' : '',
             'repos' : {}
         }
-        f = open(fileName, 'w')
+        f = open(configPath, 'w')
         f.write(json.dumps(defaultConfig, sort_keys=True, indent=4))
         f.close()
 
     # And load it...
-    with open(fileName) as file:
+    with open(configPath) as file:
         config = json.loads(file.read())
         token = config['token']
         repos = config['repos']
